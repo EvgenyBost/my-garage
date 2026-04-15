f5f1371659c71b15e32bd246fecb8c6e297b9d35
Evgeny Bostynets <jhenekb@gmail.com>
2026-04-15 14:32:24 +0300

    Add default values in DB to debugging

--- app/src/main/java/com/boss/mygarage/data/local/AppDatabase.kt
+++ app/src/main/java/com/boss/mygarage/data/local/AppDatabase.kt
@@ -3,6 +3,7 @@ package com.boss.mygarage.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.boss.mygarage.data.local.dao.NoteDao
 import com.boss.mygarage.data.local.dao.VehicleDao
 import com.boss.mygarage.data.local.entities.NoteEntity
@@ -13,4 +14,23 @@ import com.boss.mygarage.data.local.entities.VehicleEntity
 abstract class AppDatabase : RoomDatabase() {
     abstract fun vehicleDao(): VehicleDao
     abstract fun noteDao(): NoteDao
+}
+
+class DatabaseCallback : RoomDatabase.Callback() {
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        super.onCreate(db)
+        // Тестовые данные (в формате JSON для метаданных)
+        db.execSQL("""
+            INSERT INTO vehicles (name, type, year, iconId, metadata) 
+            VALUES ('Lada Vesta', 'CAR', 2021, 1, '{"Пробег":"45000 км", "Двигатель":"1.6"}')
+        """)
+        db.execSQL("""
+            INSERT INTO vehicles (name, type, year, iconId, metadata) 
+            VALUES ('Yamaha R6', 'BIKE', 2019, 2, '{"Пробег":"12000 км", "Цепь":"Обслужена"}')
+        """)
+        db.execSQL("""
+            INSERT INTO vehicles (name, type, year, iconId, metadata) 
+            VALUES ('Мотоблок Нева', 'TRACTOR', 2023, 3, '{"Моточасы":"12 ч", "Фрезы":"Установлены"}')
+        """)
+    }
 }
--- app/src/main/java/com/boss/mygarage/di/DataModule.kt
+++ app/src/main/java/com/boss/mygarage/di/DataModule.kt
@@ -2,13 +2,16 @@ package com.boss.mygarage.di
 
 import androidx.room.Room
 import com.boss.mygarage.data.local.AppDatabase
+import com.boss.mygarage.data.local.DatabaseCallback
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataModule = module {
     // Create DB singleton
     single {
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "my_garage.db").build()
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "my_garage.db")
+            .addCallback(DatabaseCallback()) //to add default items into DB and show it on the main screen
+            .build()
     }
 
     // Exposing DAOs directly (so that repositories can query them)
